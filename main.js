ded5bdb20ef2a05d3039c0c47e71123cb24a35c3
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-09-30 10:37:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,87 @@
-song = "";
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
-scoreLeftWrist = 0;
+status1 = "";
+objectDetector = "";
+object_name = "";
+objects = [];
+synth = "";
+speak_data = "";
+utterThis = "";
 
 function preload()
 {
-    song = loadSound("music.mp3");
+
 }
 
 function setup()
 {
-    canvas = createCanvas(600, 500);
-    canvas.center();
-
     video = createCapture(VIDEO);
     video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded()
-{
-    console.log("PoseNet Is Initialized!");
+    canvas = createCanvas(480, 380);
+    canvas.center();
 }
 
 function draw()
 {
-    image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 480, 380);
+    if(status1 != "")
+        {
+            objectDetector.detect(video, gotResults);
+            for (i = 0; i < objects.length; i++)
+            {
+                if(objects[i].label == object_name)
+                {
+                    objectDetector.detect(gotResults);
+                    document.getElementById("status").innerHTML = "Status : Objects detected";
+                    document.getElementById("found_or_not_found").innerHTML = object_name + " found";
+                
 
-    fill("#FF0000");
-    stroke("#FF0000");
+                    fill('#FF0000');
+                    percent = floor(objects[i].confidence * 100);
+                    text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                    noFill();
+                    stroke('#FF0000');
+                    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
-    if(scoreLeftWrist > 0.2)
-    {
-        circle(leftWristX, leftWristY, 20);
-        inNumberleftWristY = Number(leftWristY);
-        remove_decimals = floor(inNumberleftWristY);
-        volume = remove_decimals/500;
-        document.getElementById("volume").innerHTML = "Volume = " + volume;
-        song.setVolume(volume);
-    }
+                    synth = window.speechSynthesis;
+                    speak_data = object_name + "found";
+
+                    utterThis = new SpeechSynthesisUtterance(speak_data);
+                    synth.speak(utterThis);
+                }
+
+                else
+                {
+                    document.getElementById("status").innerHTML = "Status : Objects detected";
+                    document.getElementById("found_or_not_found").innerHTML = object_name + " not found";
+                    
+                    synth = window.speechSynthesis;
+                    speak_data = object_name + "not found";
+
+                    utterThis = new SpeechSynthesisUtterance(speak_data);
+                    synth.speak(utterThis);
+                }
+            }
+        }
 }
 
-function play()
+function start()
 {
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object").value;
 }
 
-function gotPoses(results)
+function modelLoaded()
 {
-    if(results.length > 0)
+    console.log("Model Loaded!");
+    status1 = true;
+}
+
+function gotResults(error, results)
+{
+    if(error) 
     {
-        console.log(results);
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("soreLeftWrist = " + scoreLeftWrist);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+        console.log(error);
     }
+    console.log(results);
+    objects = results;
 }
